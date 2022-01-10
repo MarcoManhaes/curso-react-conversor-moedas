fb49aa6c6b69af7cb67b50a3546f3b7057fca1ee
Marco Manhães <marco.manhaes@gmail.com>
2022-01-10 19:48:20 -0300

    adicionado projeto conversor de moedas curso react hooks

---
+++ src/conversor-moedas.test.js
@@ -0,0 +1,31 @@
+/* eslint-disable testing-library/await-async-query */
+/* eslint-disable testing-library/prefer-screen-queries */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ConcersorMoedas from './conversor-moedas';
+import { render, fireEvent } from '@testing-library/react';
+import "@testing-library/jest-dom/extend-expect";
+import axiosMock from 'axios';
+import ConversorMoedas from './conversor-moedas';
+
+describe('Teste do componente de conversão de moedas', () => {
+
+  it('deve renderizar o componente sem erros', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<ConcersorMoedas />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('deve simular uma conversão de moedas', async () => {
+    const { findByTestId, getByTestId } = render(<ConversorMoedas />);
+    axiosMock.get.mockResolvedValueOnce({
+      data: { success: true, rates: { BRL: 4.564292, USD: 1.101049 } }
+    });
+    fireEvent.click(getByTestId('btn-converter'));
+    const modal = await findByTestId('modal');
+    expect(axiosMock.get).toHaveBeenCalledTimes(1);
+    expect(modal).toHaveTextContent('1 BRL = 0.24 USD');
+  });
+
+});
+
